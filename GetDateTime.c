21173b04f2cbf3be9d2e4bcf4416687e0b9d9ff2
Liming SHAO <lmshao@163.com>
2016-11-25 20:32:33 +0800

    add GetDateTime

---
+++ GetDateTime.c
@@ -0,0 +1,82 @@
+/*
+ * Date & Time Function Usage Sample 
+ *
+ * time_t time(time_t *t)
+ * char *asctime(const struct tm * timeptr)
+ * char *ctime(const time_t *timep)
+ * struct tm *localtime(const time_t *timer)
+ * struct tm *gmtime(const time_t *timer)
+ * int gettimeofday(struct timeval *tv, struct timezone *tz);
+ *
+ * Copyright (c) 2016 Liming Shao <lmshao@163.com>
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+/* format: Fri Nov 25 21:06:21 2016 */
+void GetDateTime1(time_t sec)
+{
+	printf("GetDateTime1:\t");
+	printf ("%s", ctime(&sec));
+}
+
+/* format: Fri Nov 25 21:06:21 2016 */
+void GetDateTime2(time_t sec)
+{
+	printf("GetDateTime2:\t");
+	struct tm *dateTime = localtime(&sec);
+	printf ("%s", asctime(dateTime));
+}
+
+/* format: 2016-11-25 19:11:37 */
+void GetDateTime3(time_t sec)
+{
+	printf("GetDateTime3:\t");
+	struct tm *dateTime = localtime(&sec);
+	printf("%d-%02d-%02d %02d:%02d:%02d\n",
+		dateTime->tm_year + 1900,
+		dateTime->tm_mon + 1,
+		dateTime->tm_mday,
+		dateTime->tm_hour,
+		dateTime->tm_min,
+		dateTime->tm_sec);
+}
+
+/* format: 2016-11-25 11:11:37 */
+void GetDateTime4(time_t sec)
+{
+	printf("GetDateTime4:\t");
+	struct tm *dateTime = gmtime(&sec);
+	printf("%d-%02d-%02d %02d:%02d:%02d\n\n",
+		dateTime->tm_year + 1900,
+		dateTime->tm_mon + 1,
+		dateTime->tm_mday,
+		dateTime->tm_hour,
+		dateTime->tm_min,
+		dateTime->tm_sec);
+}
+
+int main()
+{
+	time_t seconds;
+	time(&seconds);		/*or use: seconds = time(NULL); */
+	printf("Seconds since the Epoch: %d\n\n", seconds);
+	
+	GetDateTime1(seconds);
+	GetDateTime2(seconds);
+	GetDateTime3(seconds);  /* Local time */
+	GetDateTime4(seconds);  /* UTC time */
+	
+	struct timeval tv;
+    struct timezone tz;
+	gettimeofday(&tv, &tz);
+	
+	/* seconds == tv.tv_sec */
+	printf("Seconds since the Epoch: %d\n", tv.tv_sec);
+/*
+	printf("tv.tv_usec: %d\n", tv.tv_usec);
+	printf("tz.tz_minuteswest: %d\n", tz.tz_minuteswest);
+	printf("tz.tz_dsttime: %d\n", tz.tz_dsttime);
+*/
+}
