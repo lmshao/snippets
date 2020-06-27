69070a37b40f585420301f8bc02ba82ea876cc0f
Liming SHAO <lmshao@163.com>
2020-06-27 23:18:34 +0800

    format

--- c/Poll_TcpServer.c
+++ c/Poll_TcpServer.c
@@ -12,25 +12,20 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-
-#include <limits.h>
 #include <poll.h>
 
-#define BACKLOG          5
-#define BUFF_SIZE      200
-#define DEFAULT_PORT  6666
-
+#define BACKLOG 5
+#define BUFF_SIZE 200
+#define DEFAULT_PORT 6666
+#define OPEN_MAX 1024
 int main(int argc, char **argv)
 {
     int SERV_PORT = DEFAULT_PORT;
 
-    if(argc > 2)
-        printf("param err:\nUsage:\n\t%s port | %s\n\n",argv[0], argv[0]);
-    if(argc == 2)
+    if (argc > 2)
+        printf("param err:\nUsage:\n\t%s port | %s\n\n", argv[0], argv[0]);
+    if (argc == 2)
         SERV_PORT = atoi(argv[1]);
 
     int i, maxi, nready;
@@ -41,26 +36,29 @@ int main(int argc, char **argv)
     struct pollfd client[OPEN_MAX];
     struct sockaddr_in cliAddr, servAddr;
 
-    if((servSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("socket err");
+    if ((servSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
         exit(1);
+    }
+
+    int optval = 1;
+    if (setsockopt(servSocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        perror("setsockopt");
+        exit(0);
     }
 
     bzero(&servAddr, sizeof(servAddr));
-    servAddr.sin_family      = AF_INET;
-    servAddr.sin_port        = htons(SERV_PORT);
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SERV_PORT);
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if(bind(servSocket,(struct sockaddr *)&servAddr,sizeof(servAddr)) < 0)
-    {
-        printf("bind err");
+    if (bind(servSocket, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+        perror("bind");
         exit(1);
     }
 
-    if(listen(servSocket, BACKLOG) < 0)
-    {
-        printf("listen err");
+    if (listen(servSocket, BACKLOG) < 0) {
+        perror("listen err");
         exit(1);
     }
     printf("Listen Port: %d\nListening ...\n", SERV_PORT);
@@ -68,75 +66,66 @@ int main(int argc, char **argv)
     client[0].fd = servSocket;
     client[0].events = POLLRDNORM;
     for (i = 1; i < OPEN_MAX; i++)
-        client[i].fd = -1;        /* -1 indicates available entry */
-    maxi = 0;                    /* max index into client[] array */
+        client[i].fd = -1; /* -1 indicates available entry */
+    maxi = 0;              /* max index into client[] array */
 
-    for ( ; ; )
-    {
-        nready = poll(client, maxi+1, -1);
-        if (nready < 0)
-        {
+    for (;;) {
+        nready = poll(client, maxi + 1, -1);
+        if (nready < 0) {
             printf("poll err");
             exit(1);
         }
 
-        if (client[0].revents & POLLRDNORM)        /* new client connection */
+        if (client[0].revents & POLLRDNORM) /* new client connection */
         {
             addrLen = sizeof(cliAddr);
-            if((cliSocket = accept(servSocket, (struct sockaddr*)&cliAddr, &addrLen)) < 0)
-            {
+            if ((cliSocket = accept(servSocket, (struct sockaddr *)&cliAddr, &addrLen)) < 0) {
                 printf("accept err");
                 exit(1);
             }
 
             for (i = 1; i < OPEN_MAX; i++)
-                if (client[i].fd < 0)
-                {
-                    client[i].fd = cliSocket;    /* save descriptor */
+                if (client[i].fd < 0) {
+                    client[i].fd = cliSocket; /* save descriptor */
                     break;
                 }
 
-            printf("\nNew client connections client[%d] %s:%d\n", i,
-                   inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port));
+            printf("\nNew client connections client[%d] %s:%d\n", i, inet_ntoa(cliAddr.sin_addr),
+                   ntohs(cliAddr.sin_port));
 
             if (i == OPEN_MAX)
                 printf("too many clients");
 
             client[i].events = POLLRDNORM;
             if (i > maxi)
-                maxi = i;    /* max index in client[] array */
+                maxi = i; /* max index in client[] array */
 
             if (--nready <= 0)
-                continue;    /* no more readable descriptors */
+                continue; /* no more readable descriptors */
         }
 
-        for (i = 1; i <= maxi; i++)        /* check all clients for data */
+        for (i = 1; i <= maxi; i++) /* check all clients for data */
         {
-            if ( (cliSocket = client[i].fd) < 0)
+            if ((cliSocket = client[i].fd) < 0)
                 continue;
 
-            if (client[i].revents & (POLLRDNORM | POLLERR))
-            {
+            if (client[i].revents & (POLLRDNORM | POLLERR)) {
                 memset(buf, 0, BUFF_SIZE);
-                if((nbytes = recv(cliSocket, buf, BUFF_SIZE, 0)) < 0)
-                {
+                nbytes = recv(cliSocket, buf, BUFF_SIZE, 0);
+                if (nbytes < 0) {
                     printf("recv err");
                     continue;
-                }
-                else if(nbytes == 0)
-                {
+                } else if (nbytes == 0) {
                     printf("client[%d] closed connection\n", i);
                     close(cliSocket);
                     client[i].fd = -1;
-                }
-                else
-                {
+                } else {
                     printf("\nFrom client[%d]\n", i);
-                    printf("Recv: %sLength: %d\n\n", buf, (int) nbytes);
+                    printf("Recv: %sLength: %d\n\n", buf, (int)nbytes);
                 }
 
                 if (--nready <= 0)
-                    break;                /* no more readable descriptors */
+                    break; /* no more readable descriptors */
             }
         }
     }
--- c/Select_TcpServer.c
+++ c/Select_TcpServer.c
@@ -1,8 +1,9 @@
 /*
- * Select Usage Sample 
- * int select (int maxfdp1, fd_set *readset, fd_set *writeset, fd_set *exceptset, const struct timeval *timeout)
- * Note:
- * As a tcp server, this program can accept multi-client connections simultaneously.
+ * Select Usage Sample
+ * int select (int maxfdp1, fd_set *readset, fd_set *writeset, fd_set *exceptset, 
+ *              const struct timeval *timeout) 
+ * Note: As a tcp server, this program can accept multi-client connections
+ * simultaneously.
  *
  * Copyright (c) 2016 Liming Shao <lmshao@163.com>
  */
@@ -12,156 +13,150 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <netdb.h>
 #include <sys/select.h>
-#include <sys/time.h>
 
-#define BACKLOG         5
-#define BUFF_SIZE     200
+#define BACKLOG 5
+#define BUFF_SIZE 200
 #define DEFAULT_PORT 6666
 
-typedef struct _CLIENT  
-{  
-    int fd;        /* client's connection descriptor */  
-    struct sockaddr_in addr;    /* client's address */
-}CLIENT;
+typedef struct {
+    int fd;                  /* client's connection descriptor */
+    struct sockaddr_in addr; /* client's address */
+} CLIENT;
 
 int main(int argc, char *argv[])
 {
     int SERVER_PORT = DEFAULT_PORT;
-    
-    if(argc > 2)
-        printf("param err:\nUsage:\n\t%s port | %s\n\n",argv[0], argv[0]);
-    if(argc == 2)
+
+    if (argc > 2)
+        printf("param err:\nUsage:\n\t%s port | %s\n\n", argv[0], argv[0]);
+    if (argc == 2)
         SERVER_PORT = atoi(argv[1]);
-    
+
     int i, maxi, maxfd, nready, nbytes;
     int servSocket, cliSocket;
     fd_set allset, rset;
     socklen_t addrLen;
     char buffer[BUFF_SIZE];
-    CLIENT client[FD_SETSIZE];        /* client info */
+    CLIENT client[FD_SETSIZE]; /* FD_SETSIZE == 1024 */
     struct sockaddr_in servAddr, cliAddr;
-    
-    if((servSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("socket err");
+
+    if ((servSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
         exit(1);
     }
 
-    bzero(&servAddr,sizeof(servAddr));
-    servAddr.sin_family      = AF_INET;
-    servAddr.sin_port        = htons(SERVER_PORT);
+    int optval = 1;
+    if (setsockopt(servSocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        perror("setsockopt");
+        exit(0);
+    }
+
+    bzero(&servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SERVER_PORT);
     servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    
-    if(bind(servSocket,(struct sockaddr *)&servAddr,sizeof(servAddr)) < 0)
-    {
-        printf("bind err");
+
+    if (bind(servSocket, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
+        printf("bind");
         exit(1);
     }
-    
-    if(listen(servSocket, BACKLOG) < 0)
-    {
+
+    if (listen(servSocket, BACKLOG) < 0) {
         printf("listen err");
         exit(1);
     }
     printf("Listen Port: %d\nListening ...\n", SERVER_PORT);
-    
+
     maxi = -1;
     maxfd = servSocket;
     for (i = 0; i < FD_SETSIZE; i++)
-        client[i].fd = -1;            /* -1 indicates available entry */
-    
-    FD_ZERO(&allset);    /* clear all bits in allset */
-    FD_SET(servSocket, &allset);
-    for( ;; )
-    {
-        rset= allset;
-/*
+        client[i].fd = -1; /* -1 indicates available entry */
+
+    FD_ZERO(&allset);            /* clear all bits in allset */
+    FD_SET(servSocket, &allset); /* add servSocket to allset */
+    for (;;) {
+        rset = allset;
+
         struct timeval timeout;
         timeout.tv_sec = 2;
         timeout.tv_usec = 0;
-*/        
-        if((nready = select(maxfd+1, &rset, NULL, NULL, NULL)) < 0)
-        {
-            printf("select err\n");
+
+        nready = select(maxfd + 1, &rset, NULL, NULL, &timeout);
+        if (nready < 0) {
+            perror("select");
             break;
         }
-        else if(nready == 0)    /* when timeout is set. */
-        {
+
+        /* when timeout is set. */
+        else if (nready == 0) {
             printf("select time out\n");
             continue;
         }
 
-        if(FD_ISSET(servSocket, &rset))        /* new client connections */
-        {
+        /* new client connections */
+        if (FD_ISSET(servSocket, &rset)) {
             addrLen = sizeof(cliAddr);
-            if((cliSocket = accept(servSocket, (struct sockaddr*)&cliAddr, &addrLen)) < 0)
-            {
-                printf("accept err");
+            if ((cliSocket = accept(servSocket, (struct sockaddr *)&cliAddr, &addrLen)) < 0) {
+                perror("accept");
                 exit(1);
             }
-            
-            printf("\nNew client connections %s:%d\n", inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port));
-            
-            for (i = 0; i < FD_SETSIZE; i++)
-                if (client[i].fd < 0)    /* save client info */
-                {
+
+            printf("\nNew client connections %s:%d\n", inet_ntoa(cliAddr.sin_addr),
+                   ntohs(cliAddr.sin_port));
+
+            /* save client info */
+            for (i = 0; i < FD_SETSIZE; i++) {
+                if (client[i].fd < 0) {
                     client[i].fd = cliSocket;
                     client[i].addr = cliAddr;
                     break;
                 }
-            
+            }
+
             if (i == FD_SETSIZE)
                 perror("too many clients");
-            
-            FD_SET(cliSocket, &allset);    /* add new descriptor to set */
+
+            /* add new descriptor to set */
+            FD_SET(cliSocket, &allset);
             if (cliSocket > maxfd)
-                maxfd = cliSocket;        /* for select */
-            
+                maxfd = cliSocket; /* for select */
+
             if (i > maxi)
-                maxi = i;                /* max index in client[] array */
+                maxi = i; /* max index in client[] array */
 
             if (--nready <= 0)
-                continue;                /* no more readable descriptors */
+                continue; /* no more readable descriptors */
         }
 
-        for (i = 0; i <= maxi; i++)        /* check all clients for data */
-        {    
+        for (i = 0; i <= maxi; i++) /* check all clients for data */
+        {
             if ((cliSocket = client[i].fd) < 0)
                 continue;
-            
-            if (FD_ISSET(cliSocket, &rset)) 
-            {
+
+            if (FD_ISSET(cliSocket, &rset)) {
                 memset(buffer, 0, BUFF_SIZE);
-                if((nbytes = recv(cliSocket, buffer, sizeof(buffer), 0)) < 0)
-                {
-                    printf("recv err");
+                nbytes = recv(cliSocket, buffer, sizeof(buffer), 0);
+                if (nbytes < 0) {
+                    perror("recv");
                     continue;
-                }
-                else if(nbytes == 0)
-                {
-                    printf("\nDisconnect %s:%d\n", 
-                        inet_ntoa(client[i].addr.sin_addr), ntohs(client[i].addr.sin_port));
+                } else if (nbytes == 0) {
+                    printf("\nDisconnect %s:%d\n", inet_ntoa(client[i].addr.sin_addr),
+                           ntohs(client[i].addr.sin_port));
                     close(cliSocket);
                     FD_CLR(cliSocket, &allset);
                     client[i].fd = -1;
-                }
-                else
-                {
-                    printf("\nFrom %s:%d\n", 
-                        inet_ntoa(client[i].addr.sin_addr), ntohs(client[i].addr.sin_port));
+                } else {
+                    printf("\nFrom %s:%d\n", inet_ntoa(client[i].addr.sin_addr),
+                           ntohs(client[i].addr.sin_port));
                     printf("Recv: %sLength: %d\n\n", buffer, nbytes);
                 }
+
                 if (--nready <= 0)
-                    break;        /* no more readable descriptors */
+                    break; /* no more readable descriptors */
             }
         }
     }
 }
-
-
